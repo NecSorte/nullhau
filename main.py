1a9f759b2ea9d340f6847f866b91fc2121d7b748
NecSorte <kevingrutherford@gmail.com>
2024-08-02 10:57:28 -0700

    updated with additional notifications for kicks

--- main.py
+++ main.py
@@ -452,6 +452,54 @@ async def round_timer():
 async def before_round_timer():
     await bot.wait_until_ready()
 
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
+
+#     if await is_on_cooldown(message.author.id):
+#         # Increment the warning count for the user
+#         warnings[message.author.id] = warnings.get(message.author.id, 0) + 1
+
+#         # Log the warning count
+#         print(f"User {message.author.name} has been warned. Total warnings: {warnings[message.author.id]}")
+
+#         # Send a warning message to the user
+#         await message.channel.send("You are sending commands too quickly. Please wait 10 seconds. Abuse will result in being kicked/banned. Malice noted.")
+
+#         # If the user has been warned three times, notify the sudo members
+#         if warnings[message.author.id] == 3:
+#             await notify_sudo_members(f"User {message.author.name} (ID: {message.author.id}) has been warned for spamming commands. They have been warned {warnings[message.author.id]} times.")
+
+#         # If the user has been warned ten times, kick the user from all guilds and notify sudo members
+#         if warnings[message.author.id] >= 10:
+#             for guild in bot.guilds:
+#                 member = guild.get_member(message.author.id)
+#                 if member:
+#                     try:
+#                         await member.kick(reason="Exceeded maximum warnings for spamming commands")
+#                         print(f"User {message.author.name} has been kicked from {guild.name} for spamming commands.")
+
+#                         # Notify the sudo members
+#                         sudo_role = discord.utils.find(lambda r: r.name.lower() == SUDO_ROLE_NAME, guild.roles)
+#                         if not sudo_role:
+#                             print(f"Sudo role not found in guild: {guild.name}")
+#                         else:
+#                             kick_message = (
+#                                 f"User {message.author.name} (ID: {message.author.id}) has been kicked for spamming commands. "
+#                                 f"They received {warnings[message.author.id]} warnings."
+#                             )
+#                             for member in guild.members:
+#                                 if sudo_role in member.roles and member != bot.user:
+#                                     try:
+#                                         await member.send(kick_message)
+#                                         print(f"Sent kick notification to {member.display_name}")
+#                                     except discord.Forbidden:
+#                                         print(f"Could not send message to {member.display_name}")
+#                     except discord.Forbidden:
+#                         print(f"Could not kick {message.author.name} from {guild.name}. Insufficient permissions.")
+#             return
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -459,20 +507,22 @@ async def on_message(message):
 
     if await is_on_cooldown(message.author.id):
         # Increment the warning count for the user
-        warnings[message.author.id] = warnings.get(message.author.id, 0) + 1
+        warnings[message.author.id] += 1
 
         # Log the warning count
         print(f"User {message.author.name} has been warned. Total warnings: {warnings[message.author.id]}")
 
         # Send a warning message to the user
-        await message.channel.send("You are sending commands too quickly. Please wait 10 seconds. Abuse will result in being kicked/banned. Malice noted.")
+        await message.channel.send("You are sending commands too quickly. Please wait 5 seconds. Abuse will result in being kicked/banned. Malice logged...")
 
         # If the user has been warned three times, notify the sudo members
         if warnings[message.author.id] == 3:
-            await notify_sudo_members(f"User {message.author.name} (ID: {message.author.id}) has been warned for spamming commands. They have been warned {warnings[message.author.id]} times.")
+            await notify_sudo_members(f"User {message.author.name} (ID: {message.author.id}) has been warned for spamming commands. They have been warned {warnings[message.author.id]} times.", message.guild)
 
         # If the user has been warned ten times, kick the user from all guilds and notify sudo members
         if warnings[message.author.id] >= 10:
+            await message.author.send("You are terminated for malice spamming. You must wait 5 seconds between each command. You may join back, but you will be banned in the future.")
+            
             for guild in bot.guilds:
                 member = guild.get_member(message.author.id)
                 if member:
@@ -496,9 +546,18 @@ async def on_message(message):
                                         print(f"Sent kick notification to {member.display_name}")
                                     except discord.Forbidden:
                                         print(f"Could not send message to {member.display_name}")
+
+                        # Notify the specified channel if CHANNEL_ID is set
+                        if CHANNEL_ID:
+                            channel = bot.get_channel(CHANNEL_ID)
+                            if channel:
+                                await channel.send(f"Employee {message.author.name} has been terminated for malice.")
+                        else:
+                            await notify_sudo_members("Please set the channel ID using /null start", guild)
                     except discord.Forbidden:
                         print(f"Could not kick {message.author.name} from {guild.name}. Insufficient permissions.")
             return
+
 
     if message.content.lower() == "yes":
         await message.author.send(get_random_response(YES_RESPONSES))
